757a3fcb5f3e9e1c3dbf034d9545466542391c3b
igoralekseev <igolexeev@mail.ru>
2023-05-17 18:40:58 +0300

    Реализовал контроллеры для формы входа и отправки сообщений

--- src/main/java/com/Cinema/controllers/MessageController.java
+++ src/main/java/com/Cinema/controllers/MessageController.java
@@ -1,4 +1,25 @@
 package com.Cinema.controllers;
 
+import com.Cinema.entyties.Message;
+import com.Cinema.services.MessagesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MessageController {
+
+    @Autowired
+    private MessagesService messagesService;
+
+    @MessageMapping("/{filmTitle}/chat")
+    @SendTo(("/film/{filmTitle}/chat/messages"))
+    public Message send(Message message)
+    {
+        Thread threadSave = new Thread(() -> messagesService.saveMessage(message));
+        threadSave.start();
+        return message;
+    }
+
 }
--- src/main/java/com/Cinema/controllers/UserController.java
+++ src/main/java/com/Cinema/controllers/UserController.java
@@ -1,4 +1,7 @@
 package com.Cinema.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
 }
